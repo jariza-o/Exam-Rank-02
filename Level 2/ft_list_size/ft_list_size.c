35f8aafc88f370a9a1db0b8320da282566fdaf5d
Juan Ariza <114615489+jariza-o@users.noreply.github.com>
2023-03-27 19:44:17 +0200

    Add files via upload

---
+++ Level 2/ft_list_size/ft_list_size.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_size.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jariza-o <jariza-o@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 16:57:43 by jariza-o          #+#    #+#             */
+/*   Updated: 2023/03/27 16:57:43 by jariza-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+int	ft_list_size(t_list *begin_list)
+{
+	if (begin_list == 0)
+		return (0);
+	else
+		return (1 + ft_list_size(begin_list->next));
+}
